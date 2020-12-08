7a10cb4d8f752ae0de20892115819b1df8fc8e1c
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-12-08 19:20:44 +0300

    Started JavaScript Course

---
+++ JS/Day1/script/app.js
@@ -0,0 +1,69 @@
+// Into to JS
+
+console.log("External JavaScript is connected")
+
+
+/*
+Web Development Course
+JavaScript Module
+*/
+
+
+//  declaring variables
+// if no value is assigned, variable will hold undefined value.
+let myName;
+let myAge;
+
+// assign values
+
+myName = "Ahmed";
+myAge = 28;
+
+
+console.log(myName);
+console.log(myAge);
+
+
+// updating variables
+
+myName = "Jama";
+myAge = 30;
+
+console.log(myName);
+console.log(myAge);
+
+
+// declaring and assigning value at sametime.
+
+let course = "Web Development";
+let session = "JavaScript Session";
+
+console.log(course, session);
+
+
+const COMPANY = "Shaqodoon";
+
+
+// Name conversion.
+
+// recommend const variable should be in Capitals
+// camelcase     myName, myAge, examResult, getData,
+// myName, course. 
+// getAge, getTotal, createUser, updateData 
+
+
+console.log( typeof myAge );
+console.log( typeof myName );
+
+// compound assignment.
+
+let sentence = "Hello";
+
+sentence  += " World";
+
+console.log(sentence);
+
+let sum = 40;
+
+sum *= 10;
+
