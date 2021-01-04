46696ca69f6acd1c32ec354857f35acc568bc93b
Mukhtar Janagale <janogale@users.noreply.github.com>
2021-01-04 20:01:41 +0300

    added challenge solutions

--- JS/Day9/session2/solution/data.js
+++ JS/Day9/session2/solution/data.js
@@ -4,6 +4,7 @@ const users = [
     name: "Ahmed Farah",
     username: "afarah",
     email: "afarah@gmail.com",
+    nationality: "Kenyan",
     address: {
       street: "Daruuraha",
       city: "Hargeisa",
@@ -36,6 +37,7 @@ const users = [
     name: "Mohamed Abdi",
     username: "mabdi",
     email: "mabdi@gmailcom",
+    nationality: "Ethiopia",
     address: {
       street: "Pepsi Area",
       city: "Hargeisa",
@@ -160,3 +162,61 @@ const users = [
     },
   },
 ];
+
+
+//Write a function called convertArrayToObject which can convert the array to a structure object.
+const students = [
+  ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]],
+  ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
+];
+
+console.log(convertArrayToObject(students))
+
+
+
+function convertArrayToObject(arr){
+  let obj = [];
+
+  for(let [name, skills, scores] of arr){
+
+   obj.push( {
+      name: name,
+      skills: skills,
+      score: scores
+    })
+
+  }
+
+  return obj;
+}
+
+
+
+console.log( convertArrayToObject(students) )
+
+
+
+const student = {
+  name: "David",
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: "HTML", level: 10 },
+      { skill: "CSS", level: 8 },
+      { skill: "JS", level: 8 },
+      { skill: "React", level: 9 },
+    ],
+    backEnd: [
+      { skill: "Node", level: 7 },
+      { skill: "GraphQL", level: 8 },
+    ],
+    dataBase: [{ skill: "MongoDB", level: 7.5 }],
+    dataScience: ["Python", "R", "D3.js"],
+  },
+};
+
+
+
+const newStudent = Object.assign({}, student, {})
+
+
--- JS/Day9/session2/solution/main.js
+++ JS/Day9/session2/solution/main.js
@@ -1,51 +1,59 @@
+let section = document.querySelector("#users");
 
-let section = document.querySelector("#users")
+for (let user of users) {
+  //   section.innerHTML +=   `
+  //   <div class="card">
+  //   <h1>${user.name}</h1>
+  //   <hr />
+  //   <p>Email: <strong>${user.email}</strong></p>
+  //   <p>Username: <strong>${user.username}</strong></p>
+  //   <h6>Works <strong>${user.company.name}</strong></h6>
+  //   <p>${user.company.slogan}</strong></p>
+  //   <br />
+  //   <address>
+  //     phone: ${user.phone} <br />
+  //     site: ${user.website} <br />
+  //     ${user.address.city} <br />
+  //     ${user.address.street}
+  //   </address>
+  // </div>
+  //   `
 
+  // object destructuring
 
-for(let user of users){
-
-
-//   section.innerHTML +=   `
-//   <div class="card">
-//   <h1>${user.name}</h1>
-//   <hr />
-//   <p>Email: <strong>${user.email}</strong></p>
-//   <p>Username: <strong>${user.username}</strong></p>
-//   <h6>Works <strong>${user.company.name}</strong></h6>
-//   <p>${user.company.slogan}</strong></p>
-//   <br />
-//   <address>
-//     phone: ${user.phone} <br />
-//     site: ${user.website} <br />
-//     ${user.address.city} <br />
-//     ${user.address.street}
-//   </address>
-// </div>
-//   `
-
-
+  const {
+    name: fullName,
+    email,
+    username,
+    website,
+    nationality = "Somali",
+    phone,
+    company: { name: companyName, slogan },
+    address: { city, street },
+  } = user;
   // create card.
-  section.insertAdjacentHTML('beforeend',  `
+  section.insertAdjacentHTML(
+    "beforeend",
+    `
   <div class="card">
-  <h1>${user.name}</h1>
+  <h1>${fullName}</h1>
   <hr />
-  <p>Email: <strong>${user.email}</strong></p>
-  <p>Username: <strong>${user.username}</strong></p>
-  <h6>Works <strong>${user.company.name}</strong></h6>
-  <p>${user.company.slogan}</strong></p>
+  <p>Email: <strong>${email}</strong></p>
+  <p>Username: <strong>${username}</strong></p>
+  <p>Nationality: <strong>${nationality}</strong></p>
+  <h6>Works <strong>${companyName}</strong></h6>
+  <p>${slogan}</strong></p>
   <br />
   <address>
-    phone: ${user.phone} <br />
-    site: ${user.website} <br />
-    ${user.address.city} <br />
-    ${user.address.street}
+    phone: ${phone} <br />
+    site: ${website} <br />
+    ${city} <br />
+    ${street}
   </address>
 </div>
   `
-
-  )
+  );
 }
-
 
 /*
 Create a div container on HTML document and create 0 to 100 numbers dynamically and append to the container div.
@@ -57,33 +65,59 @@ Prime numbers background is red
 */
 
 function isPrime(num) {
-  for(var i = 2; i < num; i++)
-    if(num % i === 0) return false;
+  for (var i = 2; i < num; i++) if (num % i === 0) return false;
   return num > 1;
 }
 
-let div = document.getElementById('numbers')
+let div = document.getElementById("numbers");
 
-for(let i = 0; i <= 100; i++){
-  let el = document.createElement('div');
-  el.textContent = i
+for (let i = 0; i <= 100; i++) {
+  let el = document.createElement("div");
+  el.textContent = i;
   el.style.fontSize = "3rem";
   el.style.textAlign = "center";
   el.style.color = "white";
+  el.style.margin = "2px";
 
-  if(i % 2 == 0){
+  if (i % 2 == 0) {
     el.style.background = "green";
   }
-  if(i % 2 != 0){
+  if (i % 2 != 0) {
     el.style.background = "yellow";
   }
 
-  if(isPrime(i)){
+  if (isPrime(i)) {
     el.style.background = "red";
+    toggleClass(el);
   }
 
+  div.appendChild(el);
 
-div.appendChild(el)
-
- // div.insertAdjacentHTML('beforeend', `<div>${i}</div>`)
+  // div.insertAdjacentHTML('beforeend', `<div>${i}</div>`)
 }
+
+function toggleClass(el) {
+  setInterval(function () {
+    el.classList.toggle("toggle");
+  }, 1000);
+}
+
+// get input values
+
+let input = document.querySelector("#input");
+let textArea = document.querySelector("#textarea");
+
+// checkbox
+let checkBox = document.querySelector('[type="checkbox"]');
+
+console.log(checkBox.checked);
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
+
+textArea.addEventListener("input", () => {
+  console.log(textArea.value);
+});
+
+console.log(input.value);
